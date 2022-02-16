63fe0507a0dc75377e807d321311962f45d33cbb
nfahmed11 <nfahmed11@gmail.com>
2022-02-15 23:39:16 -0600

    styling

--- Machine.js
+++ Machine.js
@@ -4,10 +4,12 @@ class Machine extends React.Component {
     const winner = s1 === s2 && s2 === s3;
     return (
       <div className="Machine">
-        <h2>
+        <h2 style={{ fontSize: "50px" }}>
           {s1} {s2} {s3}
         </h2>
-        <h2>{winner ? "Winner!" : "Loser!"}</h2>
+        <h2 className={winner ? "win" : "lose"}>
+          {winner ? "Winner!" : "Loser!"}
+        </h2>
       </div>
     );
   }
